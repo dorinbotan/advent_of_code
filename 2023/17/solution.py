69007aa936bfe8bc9a8c39bcbe0019cde0940853
Dorin Botan <dorinbotan@outlook.com>
2023-12-24 20:02:09 +0000

    Day 17 (part 1)

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from collections import deque
 
 sys.setrecursionlimit(500000)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-INFINITY = 2**64
+INFINITY = 2**32
 DIRECTIONS = {
     'U': 0,
     'R': 1,
@@ -14,13 +15,7 @@ DIRECTIONS = {
 
 
 def get_key(queue):
-    if len(queue) > 2 and queue[-1] == queue[-2] == queue[-3]:
-        return 8 + queue[-1]  # UUU, RRR, DDD, LLL
-
-    if len(queue) > 1 and queue[-1] == queue[-2]:
-        return 4 + queue[-1]  # _UU, _RR, _DD, _LL
-
-    return queue[-1]  # __U, __R, __D, __L
+    return queue[0] * 4 + queue[1]
 
 
 def dp(data, i, j, cache, queue, distance):
@@ -79,18 +74,77 @@ def solution1(data):
 
     for i in range(3):
         for j in DIRECTIONS.values():
-            key = get_key([j] * (i + 1))
+            key = get_key((i, j))
             cache[key] = [[INFINITY] * len(row) for row in data]
             cache[key][0][0] = 0
 
-    dp(data, 0, 1, cache, [DIRECTIONS['R']], data[0][1])
-    dp(data, 1, 0, cache, [DIRECTIONS['D']], data[1][0])
+    stack = deque()
+    stack.append((1, 0, [0, DIRECTIONS['D']], data[1][0]))
+    stack.append((0, 1, [0, DIRECTIONS['R']], data[0][1]))
+
+    while len(stack) > 0:
+        i, j, queue, distance = stack.popleft()
+
+        key = get_key(queue)
+        map = cache[key]
+
+        if map[i][j] <= distance:
+            continue
+
+        # if key > 3:
+        #     cache[key - 4][i][j] = min(cache[key - 4][i][j], distance)
+
+        # if key > 7:
+        #     cache[key - 8][i][j] = min(cache[key - 8][i][j], distance)
+
+        map[i][j] = distance
+
+        if queue[1] == DIRECTIONS['R']:
+            # dp up
+            if i > 0:
+                stack.append((i - 1, j, [0, DIRECTIONS['U']], distance + data[i - 1][j]))
+            # dp down
+            if i < len(data) - 1:
+                stack.append((i + 1, j, [0, DIRECTIONS['D']], distance + data[i + 1][j]))
+            # dp right
+            if queue[0] < 2 and j < len(data[0]) - 1:
+                stack.append((i, j + 1, [queue[0] + 1, DIRECTIONS['R']], distance + data[i][j + 1]))
+        elif queue[1] == DIRECTIONS['D']:
+            # dp left
+            if j > 0:
+                stack.append((i, j - 1, [0, DIRECTIONS['L']], distance + data[i][j - 1]))
+            # dp right
+            if j < len(data[0]) - 1:
+                stack.append((i, j + 1, [0, DIRECTIONS['R']], distance + data[i][j + 1]))
+            # dp down
+            if queue[0] < 2 and i < len(data) - 1:
+                stack.append((i + 1, j, [queue[0] + 1, DIRECTIONS['D']], distance + data[i + 1][j]))
+        elif queue[1] == DIRECTIONS['L']:
+            # dp up
+            if i > 0:
+                stack.append((i - 1, j, [0, DIRECTIONS['U']], distance + data[i - 1][j]))
+            # dp left
+            if queue[0] < 2 and j > 0:
+                stack.append((i, j - 1, [queue[0] + 1, DIRECTIONS['L']], distance + data[i][j - 1]))
+            # dp down
+            if i < len(data) - 1:
+                stack.append((i + 1, j, [0, DIRECTIONS['D']], distance + data[i + 1][j]))
+        elif queue[1] == DIRECTIONS['U']:
+            # dp left
+            if j > 0:
+                stack.append((i, j - 1, [0, DIRECTIONS['L']], distance + data[i][j - 1]))
+            # dp up
+            if queue[0] < 2 and i > 0:
+                stack.append((i - 1, j, [queue[0] + 1, DIRECTIONS['U']], distance + data[i - 1][j]))
+            # dp right
+            if j < len(data[0]) - 1:
+                stack.append((i, j + 1, [0, DIRECTIONS['R']], distance + data[i][j + 1]))
 
     result = INFINITY
 
     for i in range(3):
         for j in DIRECTIONS.values():
-            key = get_key([j] * (i + 1))
+            key = get_key((i, j))
             result = min(result, cache[key][-1][-1])
 
     return result
@@ -107,7 +161,7 @@ with open(f'{dir_path}/input.txt', 'r') as f:
     ]
 
     result1 = solution1(data)
-    print(result1)
+    print(result1)  # 953
 
     result2 = solution2(data)
     print(result2)

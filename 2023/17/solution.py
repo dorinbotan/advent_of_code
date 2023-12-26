c7b5caa9bd7cb9f8fa0fb23c59d917cb018c3d18
Dorin Botan <dorinbotan@outlook.com>
2023-12-26 01:23:07 +0000

    Day 17 (part 2)

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -1,8 +1,6 @@
 import os
-import sys
 from collections import deque
 
-sys.setrecursionlimit(500000)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 INFINITY = 2**32
@@ -15,58 +13,7 @@ DIRECTIONS = {
 
 
 def get_key(queue):
-    return queue[0] * 4 + queue[1]
-
-
-def dp(data, i, j, cache, queue, distance):
-    key = get_key(queue)
-    map = cache[key]
-
-    if map[i][j] <= distance:
-        return
-
-    map[i][j] = distance
-
-    if queue[-1] == DIRECTIONS['R']:
-        # dp up
-        if i > 0:
-            dp(data, i - 1, j, cache, [DIRECTIONS['U']], distance + data[i - 1][j])
-        # dp down
-        if i < len(data) - 1:
-            dp(data, i + 1, j, cache, [DIRECTIONS['D']], distance + data[i + 1][j])
-        # dp right
-        if key < 8 and j < len(data[0]) - 1:
-            dp(data, i, j + 1, cache, queue + [DIRECTIONS['R']], distance + data[i][j + 1])
-    elif queue[-1] == DIRECTIONS['D']:
-        # dp left
-        if j > 0:
-            dp(data, i, j - 1, cache, [DIRECTIONS['L']], distance + data[i][j - 1])
-        # dp right
-        if j < len(data[0]) - 1:
-            dp(data, i, j + 1, cache, [DIRECTIONS['R']], distance + data[i][j + 1])
-        # dp down
-        if key < 8 and i < len(data) - 1:
-            dp(data, i + 1, j, cache, queue + [DIRECTIONS['D']], distance + data[i + 1][j])
-    elif queue[-1] == DIRECTIONS['L']:
-        # dp up
-        if i > 0:
-            dp(data, i - 1, j, cache, [DIRECTIONS['U']], distance + data[i - 1][j])
-        # dp down
-        if i < len(data) - 1:
-            dp(data, i + 1, j, cache, [DIRECTIONS['D']], distance + data[i + 1][j])
-        # dp left
-        if key < 8 and j > 0:
-            dp(data, i, j - 1, cache, queue + [DIRECTIONS['L']], distance + data[i][j - 1])
-    elif queue[-1] == DIRECTIONS['U']:
-        # dp left
-        if j > 0:
-            dp(data, i, j - 1, cache, [DIRECTIONS['L']], distance + data[i][j - 1])
-        # dp right
-        if j < len(data[0]) - 1:
-            dp(data, i, j + 1, cache, [DIRECTIONS['R']], distance + data[i][j + 1])
-        # dp up
-        if key < 8 and i > 0:
-            dp(data, i - 1, j, cache, queue + [DIRECTIONS['U']], distance + data[i - 1][j])
+    return queue[0] * len(DIRECTIONS) + queue[1]
 
 
 def solution1(data):
@@ -90,12 +37,6 @@ def solution1(data):
 
         if map[i][j] <= distance:
             continue
-
-        # if key > 3:
-        #     cache[key - 4][i][j] = min(cache[key - 4][i][j], distance)
-
-        # if key > 7:
-        #     cache[key - 8][i][j] = min(cache[key - 8][i][j], distance)
 
         map[i][j] = distance
 
@@ -151,7 +92,106 @@ def solution1(data):
 
 
 def solution2(data):
-    pass
+    cache = {}
+
+    for i in range(0, 10, 1):
+        for j in DIRECTIONS.values():
+            key = get_key((i, j))
+            cache[key] = [[INFINITY] * len(row) for row in data]
+            cache[key][0][0] = 0
+
+    stack = deque()
+    stack.append((1, 0, [0, DIRECTIONS['D']], data[1][0]))
+    stack.append((0, 1, [0, DIRECTIONS['R']], data[0][1]))
+
+    while len(stack) > 0:
+        i, j, queue, distance = stack.popleft()
+
+        key = get_key(queue)
+        map = cache[key]
+
+        if map[i][j] <= distance:
+            continue
+
+        map[i][j] = distance
+
+        if queue[1] == DIRECTIONS['R']:
+            if queue[0] < 3:
+                if j < len(data[0]) - 1:
+                    # can only move forward
+                    stack.append((i, j + 1, [queue[0] + 1, DIRECTIONS['R']], distance + data[i][j + 1]))
+            else:
+                # dp up
+                if i > 0:
+                    stack.append((i - 1, j, [0, DIRECTIONS['U']], distance + data[i - 1][j]))
+                # dp down
+                if i < len(data) - 1:
+                    stack.append((i + 1, j, [0, DIRECTIONS['D']], distance + data[i + 1][j]))
+                # dp right
+                if queue[0] < 9 and j < len(data[0]) - 1:
+                    stack.append((i, j + 1, [queue[0] + 1, DIRECTIONS['R']], distance + data[i][j + 1]))
+        elif queue[1] == DIRECTIONS['D']:
+            if queue[0] < 3:
+                if i < len(data) - 1:
+                    # can only move forward
+                    stack.append((i + 1, j, [queue[0] + 1, DIRECTIONS['D']], distance + data[i + 1][j]))
+            else:
+                # dp left
+                if j > 0:
+                    stack.append((i, j - 1, [0, DIRECTIONS['L']], distance + data[i][j - 1]))
+                # dp right
+                if j < len(data[0]) - 1:
+                    stack.append((i, j + 1, [0, DIRECTIONS['R']], distance + data[i][j + 1]))
+                # dp down
+                if queue[0] < 9 and i < len(data) - 1:
+                    stack.append((i + 1, j, [queue[0] + 1, DIRECTIONS['D']], distance + data[i + 1][j]))
+        elif queue[1] == DIRECTIONS['L']:
+            if queue[0] < 3:
+                if j > 0:
+                    # can only move forward
+                    stack.append((i, j - 1, [queue[0] + 1, DIRECTIONS['L']], distance + data[i][j - 1]))
+            else:
+                # dp up
+                if i > 0:
+                    stack.append((i - 1, j, [0, DIRECTIONS['U']], distance + data[i - 1][j]))
+                # dp left
+                if queue[0] < 9 and j > 0:
+                    stack.append((i, j - 1, [queue[0] + 1, DIRECTIONS['L']], distance + data[i][j - 1]))
+                # dp down
+                if i < len(data) - 1:
+                    stack.append((i + 1, j, [0, DIRECTIONS['D']], distance + data[i + 1][j]))
+        elif queue[1] == DIRECTIONS['U']:
+            if queue[0] < 3:
+                if i > 0:
+                    # can only move forward
+                    stack.append((i - 1, j, [queue[0] + 1, DIRECTIONS['U']], distance + data[i - 1][j]))
+            else:
+                # dp left
+                if j > 0:
+                    stack.append((i, j - 1, [0, DIRECTIONS['L']], distance + data[i][j - 1]))
+                # dp up
+                if queue[0] < 9 and i > 0:
+                    stack.append((i - 1, j, [queue[0] + 1, DIRECTIONS['U']], distance + data[i - 1][j]))
+                # dp right
+                if j < len(data[0]) - 1:
+                    stack.append((i, j + 1, [0, DIRECTIONS['R']], distance + data[i][j + 1]))
+
+    result = INFINITY
+
+    for i in range(0, 10, 1):
+        for j in DIRECTIONS.values():
+            key = get_key((i, j))
+            result = min(result, cache[key][-1][-1])
+
+    graph = [[INFINITY] * len(row) for row in data]
+    for x in range(len(data)):
+        for y in range(len(data[0])):
+            for i in range(0, 10, 1):
+                for j in DIRECTIONS.values():
+                    key = get_key((i, j))
+                    graph[x][y] = min(graph[x][y], cache[key][x][y])
+
+    return result
 
 
 with open(f'{dir_path}/input.txt', 'r') as f:
@@ -164,4 +204,4 @@ with open(f'{dir_path}/input.txt', 'r') as f:
     print(result1)  # 953
 
     result2 = solution2(data)
-    print(result2)
+    print(result2)  # 1180
